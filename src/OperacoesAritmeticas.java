995037525fd2302bff2cde1270af880651291d1f
Jorge-Cammarota <Jorge-Cammarota>
2023-09-26 11:54:02 -0300

    operacoes

---
+++ src/OperacoesAritmeticas.java
@@ -0,0 +1,27 @@
+public class OperacoesAritmeticas {
+    public static void main(String[] args) {
+        int minhaIdade = 38;
+        int suaIdade = 25;
+
+        // Adição
+        int totalidade = minhaIdade + 100;
+        System.out.println("Soma das idades: " + totalidade);
+
+        // Subtração
+        int diferencaIdades = minhaIdade - suaIdade;
+        System.out.println("Diferença das idades: " + diferencaIdades);
+
+        //Multiplicação
+        int dobroDaIdade = 2 * suaIdade;
+        System.out.println("Dobro da sua idade: " + dobroDaIdade);
+
+        //Divisão
+        int metadeDaIdade = suaIdade / 2;
+        System.out.println("Metade da sua idade: " + metadeDaIdade);
+
+        //Módulo
+        int restoDivisao = 7 % 2;
+        System.out.println("Resto da divisão (módulo)" + restoDivisao);
+
+}
+ }
